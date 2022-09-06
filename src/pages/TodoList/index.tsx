386d4f7e8640fcd4ccd86971612f7022bb33be62
Albert Arakelyan <albert.arakelyan@esterox.am>
2022-09-06 21:08:15 +0400

    initial commit

---
+++ src/pages/TodoList/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// Components
+import CreateForm from '../../components/pages/TodoList/CreateForm';
+
+// Styles
+import styles from './TodoList.module.scss';
+
+
+const TodoList = () => {
+  return (
+    <div className={styles['todo-list']}>
+      <div className="container">
+        <div className={styles['todo-list__content']}>
+          <CreateForm />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
